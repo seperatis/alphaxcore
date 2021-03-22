98a5e622e5966b60a3bd197f6c01b700868deb17
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-03-22 02:23:27 +0100

    Update and rename YesPowerB2.cs to YespowerB2.cs

--- src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerB2.cs
+++ src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerB2.cs
@@ -26,7 +26,7 @@ using Alphaxcore.Native;
 
 namespace Alphaxcore.Crypto.Hashing.Algorithms
 {
-    public unsafe class YesPowerB2 : IHashAlgorithm
+    public unsafe class YespowerB2 : IHashAlgorithm
     {
         public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
         {
