dc38644ab547f8bd913adbf8b795fc7f2acd6a11
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-03-22 02:24:24 +0100

    Update and rename YesPowerIOTS.cs to YespowerIOTS.cs

--- src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerIOTS.cs
+++ src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerIOTS.cs
@@ -26,7 +26,7 @@ using Alphaxcore.Native;
 
 namespace Alphaxcore.Crypto.Hashing.Algorithms
 {
-    public unsafe class YesPowerIOTS : IHashAlgorithm
+    public unsafe class YespowerIOTS : IHashAlgorithm
     {
         public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
         {
