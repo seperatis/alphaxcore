b76da50ba0f9aa3633044366dedf09fc89ce2c8b
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-03-22 02:36:33 +0100

    Update and rename YesPower_Urx.cs to YespowerURX.cs

--- src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerURX.cs
+++ src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerURX.cs
@@ -26,7 +26,7 @@ using Alphaxcore.Native;
 
 namespace Alphaxcore.Crypto.Hashing.Algorithms
 {
-    public unsafe class YesPower_Urx : IHashAlgorithm
+    public unsafe class YespowerURX : IHashAlgorithm
     {
         public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
         {
