78cec451a66aacb030da522dbbdb21661baed6dc
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-03-22 02:31:35 +0100

    Update and rename YesPower_R16.cs to YespowerR16.cs

--- src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerR16.cs
+++ src/Alphaxcore/Crypto/Hashing/Algorithms/YespowerR16.cs
@@ -26,7 +26,7 @@ using Alphaxcore.Native;
 
 namespace Alphaxcore.Crypto.Hashing.Algorithms
 {
-    public unsafe class YesPower_R16 : IHashAlgorithm
+    public unsafe class YespowerR16 : IHashAlgorithm
     {
         public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
         {
