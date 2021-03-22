54ec0bc2f7bd934efaf6b7e46e7e135e6e3556fe
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-03-22 02:33:43 +0100

    Update and rename YesPower_Tide.cs to YessowerTIDE.cs

--- src/Alphaxcore/Crypto/Hashing/Algorithms/YessowerTIDE.cs
+++ src/Alphaxcore/Crypto/Hashing/Algorithms/YessowerTIDE.cs
@@ -26,7 +26,7 @@ using Alphaxcore.Native;
 
 namespace Alphaxcore.Crypto.Hashing.Algorithms
 {
-    public unsafe class YesPower_Tide : IHashAlgorithm
+    public unsafe class YespowerTIDE : IHashAlgorithm
     {
         public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
         {
