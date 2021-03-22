bd27d8e455dd7ac9dd97b8669978ac87c86689ac
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-03-22 02:30:53 +0100

    Update and rename VerusHash.cs to Verushash.cs

--- src/Alphaxcore/Crypto/Hashing/Algorithms/Verushash.cs
+++ src/Alphaxcore/Crypto/Hashing/Algorithms/Verushash.cs
@@ -26,7 +26,7 @@ using Alphaxcore.Native;
 
 namespace Alphaxcore.Crypto.Hashing.Algorithms
 {
-    public unsafe class VerusHash : IHashAlgorithm
+    public unsafe class Verushash : IHashAlgorithm
     {
         public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
         {
