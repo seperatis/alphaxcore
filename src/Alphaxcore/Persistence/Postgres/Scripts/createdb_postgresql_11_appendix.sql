cb50a597f47c1ea57cf9193696bf64640276e252
CryptoCyberCid <67390536+CryptoCyberCid@users.noreply.github.com>
2021-05-15 02:54:39 +0200

    Update createdb_postgresql_11_appendix.sql

--- src/Alphaxcore/Persistence/Postgres/Scripts/createdb_postgresql_11_appendix.sql
+++ src/Alphaxcore/Persistence/Postgres/Scripts/createdb_postgresql_11_appendix.sql
@@ -1,4 +1,4 @@
-SET ROLE miningcore;
+SET ROLE alphaxcore;
 
 DROP TABLE shares;
 
@@ -12,9 +12,9 @@ CREATE TABLE shares
 	worker TEXT NULL,
 	useragent TEXT NULL,
 	ipaddress TEXT NOT NULL,
-        source TEXT NULL,
+    source TEXT NULL,
 	created TIMESTAMP NOT NULL
 ) PARTITION BY LIST (poolid);
 
-CREATE INDEX IDX_SHARES_CREATED ON SHARES(created);
-CREATE INDEX IDX_SHARES_MINER_DIFFICULTY on SHARES(miner, difficulty);
+CREATE INDEX IDX_SHARES_CREATED on shares(created);
+CREATE INDEX IDX_SHARES_MINER_DIFFICULTY on shares(miner, difficulty);
